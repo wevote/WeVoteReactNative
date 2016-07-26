ecf0e4468ebb50911cce10957806266c17378f0f
Rob Simpson <rsimpson2@me.com>
2016-07-26 03:40:47 -0400

    Basic boilerplate for iOS header
    This is a basic header that can be populated with more options and data
    that one would see similar to the web app.

--- index.ios.js
+++ index.ios.js
@@ -14,7 +14,8 @@ import {
   View,
   Navigator
 } from 'react-native';
-import Position_list from './assets/stubs/position_list'
+import Position_list from './assets/stubs/position_list';
+import Header from './components/header';
 
 let CandidateCard = require('./components/candidateCard').default;
 let ListCard = require('./components/listCard').default;
@@ -59,11 +60,11 @@ class WeVoteReactNative extends Component {
         },
       });
       let responseJson = await response.json();
-
+    
       // Put the contents of the organization JSON into the state so the app can react
       // console.log("responseJson: ", responseJson);
       this.setState({candidate: responseJson});
-
+  
     } catch(error) {
       console.error(error);
     }
@@ -200,6 +201,7 @@ class WeVoteReactNative extends Component {
         });
 
         return <ScrollView>
+          <Header></Header>
           <View style={styles.container}>
             <OrganizationalCard organization_name={ organization_name }
                                 organization_photo_url={ organization_photo_url }
