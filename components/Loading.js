10b25a6f5d37d174fec239864b3d66dd879c70d8
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2016-07-26 04:14:07 -0700

    Completely updated WeVoteReactNative "Getting Started" documentation so we can welcome new developers!

---
+++ components/Loading.js
@@ -0,0 +1,47 @@
+import React, { Component, } from 'react'
+import { StyleSheet, Text, View, ActivityIndicatorIOS } from 'react-native'
+
+class Loading extends Component {
+
+  static propTypes = {}
+
+  static defaultProps = {}
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={[styles.loading_screen]}>
+          <Text style={styles.loading_text}>Loading {this.props.children}</Text>
+          <ActivityIndicatorIOS style={{alignSelf: 'center'}} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  loading_screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#337ec9',
+  },
+  loading_text: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize:60
+  }
+})
+
+export default Loading
