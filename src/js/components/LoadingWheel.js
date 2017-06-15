008ce2a5418f5845c777906b998b1def91b4b5b7
RohanVB <rohan.vbh@gmail.com>
2017-06-15 17:30:15 +0530

    Adds ActivityIndicator

--- src/js/components/LoadingWheel.js
+++ src/js/components/LoadingWheel.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { Component } from 'react';
+import { ActivityIndicator, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const LoadingWheel =
-  <div className="u-loading-spinner__wrapper">
-    <div className="u-loading-spinner">Loading...</div>
-  </div>;
+class LoadingWheel extends Component {
 
-export default LoadingWheel;
+   state = { animating: true }
+
+   closeActivityIndicator = () => setTimeout(() => this.setState({ animating: false }), 6000)
+
+   componentDidMount = () => this.closeActivityIndicator()
+
+   render() {
+      const animating = this.state.animating
+      return (
+         <View style = {styles.container}>
+            <ActivityIndicator
+               animating = {animating}
+               color = '#bc2b78'
+               size = "large"
+               style = {styles.activityIndicator}
+            />
+         </View>
+      )
+   }
+}
+
+export default LoadingWheel
+
+const styles = StyleSheet.create ({
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 70
+   },
+  
+   activityIndicator: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 80
+   }
+})
