c80c47284d8b133665aa78533b58c0dd71742e61
stevepodell <steve@podell.com>
2017-09-13 16:22:43 -0700

    Don't attempt to send a null value or a promise, for a cookie over http Some of the early setup for    https://github.com/fullstackreact/react-native-oauth Plus some Android/Java changes for react-native-oauth

--- android/app/src/main/java/com/wevotereactnative/MainApplication.java
+++ android/app/src/main/java/com/wevotereactnative/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import com.psykar.cookiemanager.CookieManagerPackage;
 
 import com.facebook.react.ReactApplication;
+import io.fullstack.oauth.OAuthManagerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OAuthManagerPackage(),
             new VectorIconsPackage(),
             new CookieManagerPackage()
       );
