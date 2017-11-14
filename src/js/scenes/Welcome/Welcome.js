9b30f35c7e762a1d721d651a96c7dd30f15b6bde
stevepodell <steve@podell.com>
2017-11-13 16:57:38 -0800

    The top menu now looks more like the webapp. Added a new Welcome scene.

---
+++ src/js/scenes/Welcome/Welcome.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import styles from "../../stylesheets/BaseStyles"
+const logging = require("../../utils/logging");
+
+/*
+  This is just a placeholder for whatever will handle Welcome, feel free to use it or replace it.
+  Steve Nov 13, 2017
+ */
+
+export default class Welcome extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+     };
+
+  }
+
+  static onEnter = () => {
+    logging.rnrfLog("onEnter to Welcome: currentScene = " + Actions.currentScene);
+    Actions.refresh({dummy: 'hello'});  // triggers componentWillReceiveProps
+  };
+
+  static onExit = () => {
+    logging.rnrfLog("onExit from Welcome: currentScene = " + Actions.currentScene);
+  };
+
+  componentWillReceiveProps(nextProps) {
+  }
+
+  // Doesn't work in react-native? // componentDidMount () {
+  componentWillMount () {
+    console.log("Welcome ++++ MOUNT currentScene = " + Actions.currentScene);
+  }
+
+  componentWillUnmount () {
+    console.log("Welcome ---- UN mount");
+  }
+
+
+  render () {
+    if (Actions.currentScene !== "welcome") {
+      logging.renderLog("Welcome", "when NOT CURRENT, scene  = " + Actions.currentScene);
+      return null;
+    }
+
+    logging.renderLog("Welcome", "scene = " + Actions.currentScene);
+
+    return <View style={styles.outerGrayPane} >
+        <View style={styles.innerWhitePane} >
+          <View>
+            <Text style={styles.titleText} >Hello world!</Text>
+          </View>
+        </View>
+      </View>;
+  } // render
+} // class
